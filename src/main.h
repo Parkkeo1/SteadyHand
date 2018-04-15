374410a3badf3af62fadbc5b13ca2e5c306589cd
Parkkeo1 <keonp2@illinois.edu>
2018-04-14 20:18:48 -0500

    Removing legacy files for now, because I'm now recording mouse movements in a simpler way.

--- src/main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <Windows.h>
-
-LRESULT CALLBACK WndProc(HWND window_handle, UINT message, WPARAM w_param, LPARAM l_param);
-
-#endif // !MAIN_H
