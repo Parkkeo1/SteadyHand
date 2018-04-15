c46da9ac3848da4e16738647bc1c6600bae560fb
Parkkeo1 <keonp2@illinois.edu>
2018-04-14 20:03:39 -0500

    refactored LoadPatternFromFile() to be cleaner code.

--- src/pattern_parser.cpp
+++ src/pattern_parser.cpp
@@ -7,25 +7,19 @@ PatternObject LoadPatternFromFile(std::string filename, Weapon gun_name) {
 
 	if (pattern_file.is_open()) {
 		long long prev_time = 0;
+		bool is_first_line = true;
 
-		if (getline(pattern_file, file_line)) {
-			std::istringstream in_sstream(file_line);
-			std::vector<std::string> first_xy_info(std::istream_iterator<std::string>{in_sstream}, 
-				                                   std::istream_iterator<std::string>());
-			prev_time = std::stoll(first_xy_info[0]);
-			spray_pattern.movement_coords.push_back({ 0, std::stoi(first_xy_info[1]), std::stoi(first_xy_info[2]) });
-		} else {
-			// if the first line is empty, then just return immediately. No point in going further.
-			return spray_pattern;
-		}
-
+		// TODO: test this fix.
 		while (getline(pattern_file, file_line)) {
 			std::istringstream in_sstream(file_line);
 			std::vector<std::string> xy_info(std::istream_iterator<std::string>{in_sstream}, 
 				                             std::istream_iterator<std::string>());
+			if (is_first_line) {
+				prev_time = std::stoll(xy_info[0]);
+				is_first_line = false;
+			}
 
-			spray_pattern.movement_coords.push_back({ std::stoll(xy_info[0]) - prev_time, 
-				                                      std::stoi(xy_info[1]), std::stoi(xy_info[2]) });
+			spray_pattern.movement_coords.push_back({ std::stoll(xy_info[0]) - prev_time, std::stoi(xy_info[1]), std::stoi(xy_info[2]) });
 			spray_pattern.total_x_travel += std::stoi(xy_info[1]);
 			spray_pattern.total_y_travel += std::stoi(xy_info[2]);
 			prev_time = std::stoll(xy_info[0]);
