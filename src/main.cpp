74c40ecd89ee529d132fcbb777c29c8dc92f9e80
Parkkeo1 <keonp2@illinois.edu>
2018-05-03 18:16:55 -0500

    first commit to new public fork. testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 #include <Windows.h>
 
-//========================================================================
 int WINAPI WinMain(HINSTANCE h_inst, HINSTANCE h_prev_inst, LPSTR lp_cmd_line, int n_cmd_show) {
 	ofSetupOpenGL(540, 400, OF_WINDOW);
 	ofApp *steady_hand = new ofApp();
